9d980d58a30a7477d9fc35ac11203aacbae2e66b
solomongamid <solomongamid@users.noreply.github.com>
2016-04-21 17:53:29 +0200

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -7,7 +7,7 @@
 var HelloWorld = function() {};
 
 HelloWorld.prototype.hello = function(input) {
-  if (typeof(input) === 'undefined') {
+  if (!input) {
     return "Hello, World!";
   } else {
     return 'Hello, ' + input + '!';
