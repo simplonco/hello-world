0829e373f77c65625480a25579f3c995e597f6fe
aboualnaser <aboualnaser@gmail.com>
2016-04-27 11:00:56 +0200

    first

--- hello-world.js
+++ hello-world.js
@@ -6,12 +6,13 @@
 
 'use strict';
 
-var HelloWorld = function() {};
+var HelloWorld = function () {};
 
-HelloWorld.prototype.hello = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+HelloWorld.prototype.hello = function (input) {
+  if (input !="") {
+    return "Hello " + input;
+  } else {
+     return "Hello, World!" }
 };
 
 module.exports = HelloWorld;
